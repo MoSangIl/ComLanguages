aefc8c984487a73473b3533354e48308ec21da62
Mosangil <mosang1230@gmail.com>
2020-07-14 15:23:42 +0900

    Commit First Study About Variable Types

---
+++ python/자료형/practice.py
@@ -0,0 +1,29 @@
+print(5)
+print(-10)
+print(3.14)
+print(10000)
+print(5+3)
+print(2*8)
+print(3*(3+1))
+print('풍선')
+print("나비")
+
+# 참 / 거짓 
+print(5 > 10)
+print(True)
+print(not True)
+print(not False)
+print(not (5 > 10))
+
+# 변수
+animal = "고슴도치"
+name = "도도치"
+age = 2
+hobby = "가시 뿜기"
+is_adult = age >= 3
+print("우리 집 " + animal + "의 이름은 " + name + "예요")
+#print(name + "는 " + str(age) + "살이며, " + hobby +"를 아주 좋아해요")
+print(name, "는 ", age, "살이며, ", hobby, "를 아주 좋아해요")
+print(name + "는 어른일까요? " + str(is_adult))
+
+# 
